62ad3a3d5dd12b408332c3b99c998cc5d03c11e1
Bartlomiej Takuski <Bartlomiej.Takuski@grainger.com>
2020-04-20 19:37:19 +0000

    Added remote config file

---
+++ wdio.remote.conf.js
@@ -0,0 +1,35 @@
+const base = require('./wdio.conf.js');
+
+exports.config = Object.assign(base.config, {
+    hostname: '', // Host IP 
+    port: 4444,
+    path: '/wd/hub',
+    maxInstances: 1,
+
+    capabilities: [{
+        maxInstances: ,
+        acceptInsecureCerts: true,
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--ignore-certificate-errors'],
+        },
+        'zal:recordVideo': 'false',
+        'zal:name': 'wdio-docker-test',
+        'zal:build': 'WebDriverIO',
+    }],
+    services: ['selenium-standalone', 'shared-store'],
+
+    // eslint-disable-next-line object-shorthand, no-unused-vars, func-names
+    beforeSession: function (config, capabilities, specs) {
+        const specName = String(specs)
+            .split('/')
+            .pop();
+        const buildName = String(specs)
+            .split('/', 9)
+            .pop();
+        capabilities['zal:build'] = buildName;
+        capabilities['zal:name'] = specName;
+    },
+});
+
+
